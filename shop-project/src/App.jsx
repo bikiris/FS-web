c8eb682c32f0261e5575f4429959c085c71cacd9
bikiris <a664347635@gmail.com>
2025-04-26 15:15:37 -0400

    Add motion

--- shop-project/src/App.jsx
+++ shop-project/src/App.jsx
@@ -1,5 +1,7 @@
 import Navbar from "./components/Navbar";
 
+import { motion } from "motion/react";
+
 const App = () => {
   return (
     <>
@@ -7,8 +9,18 @@ const App = () => {
       <section className='flex justify-center items-center h-screen px-8 pt-16 gap-6 flex-wrap lg:flex-nowrap bg-[url(https://images.unsplash.com/photo-1607082348824-0a96f2a4b9da?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] 
         bg-cover bg-center'>
         <div className="flex flex-col gap-4 text-center">
-          <h1 className="text-8xl text-red-400 text-outline-black">Shop Project</h1>
-          <p className="text-2xl">Welcome to my store introduction</p>
+          <motion.h1 className="text-8xl text-red-400 text-outline-black"
+            initial={{ opacity: 0, y: -50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1 }}>
+            Shop Project
+          </motion.h1>
+          <motion.p className="text-2xl"
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 2 }}>
+            Welcome to my store introduction
+          </motion.p>
         </div>
      
       </section>
--- shop-project/src/components/ProductItem.jsx
+++ shop-project/src/components/ProductItem.jsx
@@ -1,11 +1,28 @@
+import { useNavigate } from "react-router-dom";
+
+import { motion } from 'motion/react';
+
 const ProductItem = ({imgLink, productName, price}) => {
+
+  const navigate = useNavigate();
+  const openProductPage = () => {
+    navigate('/products/1');
+  }
+
   return (
-    <div className="flex flex-col p-2">
+    <motion.div className="flex flex-col p-2 hover:cursor-pointer" 
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 0.5 }}
+      viewport={{ once: true }}
+     
+  
+      onClick={openProductPage} >
       <img src= {imgLink} 
         alt='product image'/>
       <h1 className="font-bold text-lg">{productName}</h1>
       <p>${price}</p>
-    </div>
+    </motion.div>
   )
 }
 
